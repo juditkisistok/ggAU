b966eec71cf74395b1d0ae3bb1fc21510cd0f51d
juditkisistok <judit.kisistok@gmail.com>
2022-12-07 14:44:54 +0100

    fixing continuous fill scale and adding tests

--- R/scale_fill_au.R
+++ R/scale_fill_au.R
@@ -16,7 +16,26 @@ scale_fill_au <- function(aesthetics = "fill", style = "light", reverse = FALSE,
     ggplot2::scale_fill_manual(aesthetics, values = unname(palette),
                                breaks, na.value = na.value)
   } else {
-    scale_fill_gradientn(colours = palette(256), ...)
+    ggplot2::scale_fill_gradientn(colours = palette, ...)
   }
 }
+
+# Dummy data
+dummy_data <- expand.grid(x = paste0("var_", seq(1, 10)),
+                          y = paste0("var_", seq(11, 20)))
+dummy_data$z <- runif(100, -1, 1)
+
+# Heatmap
+ggplot(dummy_data, aes(x, y, fill = z)) +
+  geom_tile() +
+  scale_fill_au(style = "hotandcold")
+
+ggplot(dummy_data, aes(x, y, fill= z)) +
+  geom_tile() +
+  scale_fill_au(style = "custom", colors = c("yellow", "white", "darkblue"))
+
+ggplot(iris, aes(x = Species, y = Species, fill = Sepal.Length)) +
+  geom_tile()  +
+  scale_fill_au(style = "hotandcold")
+
 
--- tests/testthat/test-scale_fill_au.R
+++ tests/testthat/test-scale_fill_au.R
@@ -17,3 +17,59 @@ test_that("custom discrete scale works", {
 
   expect_true(all(unique(g$data[[1]]["fill"]) == c("#003d73", "#e2001a", "#fabb00")))
 })
+
+test_that("continuous built-in scale works", {
+  dummy_data <- expand.grid(x = paste0("var_", seq(1, 10)),
+                            y = paste0("var_", seq(11, 20)))
+  dummy_data$z <- runif(100, -1, 1)
+
+  p = ggplot(dummy_data, aes(x, y, fill = z)) +
+    geom_tile() +
+    scale_fill_au(style = "hotandcold")
+
+  g = ggplot_build(p)
+
+  expect_equal(nrow(g$data[[1]]["fill"]), 100)
+})
+
+test_that("continuous custom scale works", {
+  dummy_data <- expand.grid(x = paste0("var_", seq(1, 10)),
+                            y = paste0("var_", seq(11, 20)))
+  dummy_data$z <- runif(100, -1, 1)
+
+  p = ggplot(dummy_data, aes(x, y, fill = z)) +
+    geom_tile() +
+    scale_fill_au(style = "custom", colors = c("yellow", "white", "red"))
+
+  g = ggplot_build(p)
+
+  expect_equal(nrow(g$data[[1]]["fill"]), 100)
+})
+
+test_that("continuous built-in scale applies correct colors", {
+  dummy_data <- expand.grid(x = paste0("var_", seq(1, 10)),
+                            y = paste0("var_", seq(11, 20)))
+  dummy_data$z <- rep_len(c(-1, 0, 1), 100)
+
+  p = ggplot(dummy_data, aes(x, y, fill = z)) +
+    geom_tile() +
+    scale_fill_au(style = "hotandcold")
+
+  g = ggplot_build(p)
+
+  expect_true(all(unique(g$data[[1]]["fill"]) == c("#003D73", "#FFFFFF", "#E2001A")))
+})
+
+test_that("continuous custom scale applies correct colors", {
+  dummy_data <- expand.grid(x = paste0("var_", seq(1, 10)),
+                            y = paste0("var_", seq(11, 20)))
+  dummy_data$z <- rep_len(c(-1, 0, 1), 100)
+
+  p = ggplot(dummy_data, aes(x, y, fill = z)) +
+    geom_tile() +
+    scale_fill_au(style = "custom", colors = c("yellow", "white", "green"))
+
+  g = ggplot_build(p)
+
+  expect_true(all(unique(g$data[[1]]["fill"]) == c("#FABB00", "#FFFFFF", "#8BAD3F")))
+})
