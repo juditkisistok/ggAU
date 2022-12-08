d7391f4c1087dee40a8abc7bdfe833bc3de27c5f
juditkisistok <judit.kisistok@gmail.com>
2022-12-08 11:50:32 +0100

    documenting au_color_palette

--- R/au_color_palette.R
+++ R/au_color_palette.R
@@ -1,10 +1,18 @@
-#' Title
+#' Color palette generator
 #'
-#' @return
+#' @param style String, name of a pre-defined color palette (`light`, `dark`,
+#' `hotandcold`, `hotandcold_dark`), or `custom`. The default palette is `light`.
+#' @param reverse Boolean, TRUE will reverse the color order.
+#' @param colors Vector of AU color names. Only used when `style = "custom"` is passed in.
+#'
+#' @return A named vector with a pre-defined or custom color palette.
 #' @export
 #'
 #' @examples
-au_color_palette = function(style = "light", reverse = FALSE, colors = NA, ...) {
+#' au_color_palette("hotandcold")
+#'
+#' au_color_palette("custom", colors = c("yellow", "magenta", "white"))
+au_color_palette = function(style = "light", reverse = FALSE, colors = NA) {
   palettes = list(
     dark = au_colors("darkblue", "darkpurple", "darkcyan",
                      "darkturkis", "darkgreen", "darkorange",
