0b0fb156e85796eab6533b2047e253b437fa91bf
juditkisistok <judit.kisistok@gmail.com>
2022-12-12 14:18:07 +0100

    unit testing au_color_palette

--- tests/testthat/test-au_color_palette.R
+++ tests/testthat/test-au_color_palette.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("function returns a vector", {
+  expect_equal(class(au_color_palette()), "character")
+})
+
+test_that("function returns a named vector", {
+  expect_equal(names(au_color_palette()),
+               c("blue", "purple", "cyan", "turkis", "green",
+                 "yellow", "red", "magenta"))
+})
+
+test_that("function returns the correct colors", {
+  expect_equal(unname(au_color_palette(style = "test")),
+               c("#003d73", "#fabb00", "#e2001a"))
+})
+
+test_that("reversing the palette works", {
+  expect_equal(unname(au_color_palette(style = "test", reverse = T)),
+               c("#e2001a", "#fabb00", "#003d73"))
 })
