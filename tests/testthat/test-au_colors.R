9dd9865cd5b3878cced58a1cc74fb1f69af382ec
juditkisistok <judit.kisistok@gmail.com>
2022-12-12 14:12:42 +0100

    unit testing au_colors

---
+++ tests/testthat/test-au_colors.R
@@ -0,0 +1,20 @@
+test_that("function returns a vector", {
+  expect_equal(class(au_colors()), "character")
+})
+
+test_that("function returns the correct names", {
+  expect_equal(names(au_colors("red")), "red")
+})
+
+test_that("function returns the correct number of colors", {
+  expect_equal(length(au_colors("red", "blue", "green")), 3)
+})
+
+test_that("function returns the correct hex codes", {
+  expect_equal(unname(au_colors("red", "blue", "green")),
+               c("#e2001a", "#003d73", "#8bad3f"))
+})
+
+test_that("if no argument is passed in, all colors are returned", {
+  expect_equal(length(au_colors()), 21)
+})
