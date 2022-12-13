332993d6447c98c577c2b6b17b848608403470cd
juditkisistok <judit.kisistok@gmail.com>
2022-12-13 15:21:27 +0100

    adding the pipe

--- R/violinplotter.R
+++ R/violinplotter.R
@@ -4,6 +4,13 @@
 #' @export
 #'
 #' @examples
-violinplotter = function() {
+violinplotter = function(data, x_val, y_val, x_lab = "x", y_lab = "y", title = "",
+                         filter = F, filter_col = NA, filter_val = NA,
+                         comp_vec = NA, col_vec = au_colors()) {
+  if (filter == T) {
+    data = dplyr::filter(data, get(filter_col) == filter_val)
+  }
 
 }
+
+violinplotter(iris, "Species", "Petal.Width")
