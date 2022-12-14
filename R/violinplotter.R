6eb1b5081894c26bdf6e29ff53e1a61e8c5c2633
juditkisistok <judit.kisistok@gmail.com>
2022-12-14 11:14:09 +0100

    making the sample size count optional in violinplotter

--- R/violinplotter.R
+++ R/violinplotter.R
@@ -8,17 +8,10 @@ violinplotter = function(data, x_val, y_val, x_lab = "x", y_lab = "y", title = "
                          filter = F, filter_col = NA, filter_val = NA,
                          comp_vec = NA, col_vec = au_colors(),
                          fill_vec = au_colors(), col_style = "light",
-                         fill_style = "light") {
+                         fill_style = "light", display_n = T) {
   if (filter == T) {
     data = dplyr::filter(data, get(filter_col) == filter_val)
   }
-
-  # counts the number of data points for annotation purposes
-  stat = data %>%
-    dplyr::select(x_val, y_val) %>%
-    na.omit() %>%
-    dplyr::count() %>%
-    dplyr::mutate(lab = paste0('n = ', n))
 
   p = ggplot2::ggplot(data, ggplot2::aes_string(x = x_val, y = y_val)) +
     ggforce::geom_sina(ggplot2::aes_string(color = x_val))+
@@ -30,8 +23,22 @@ violinplotter = function(data, x_val, y_val, x_lab = "x", y_lab = "y", title = "
     ggplot2::theme(legend.position = 'none',
           plot.title = ggplot2::element_text(hjust = 0.5)) +
     scale_color_au(colors = col_vec, style = col_style, discrete = T) +
-    scale_fill_au(colors = fill_vec, style = fill_style, discrete = T) +
-    ggplot2::ggtitle(paste0(title, ', ', stat$lab))
+    scale_fill_au(colors = fill_vec, style = fill_style, discrete = T)
+
+  if (display_n) {
+    # counts the number of data points for annotation purposes
+    stat = data %>%
+      dplyr::select(x_val, y_val) %>%
+      na.omit() %>%
+      dplyr::count() %>%
+      dplyr::mutate(lab = paste0('n = ', n))
+
+    p = p +
+      ggplot2::ggtitle(paste0(title, ', ', stat$lab))
+  } else {
+    p = p +
+      ggplot2::ggtitle(title)
+  }
 
   if (!is.na(comp_vec[1])) {
     p = p + ggpubr::stat_compare_means(comparisons = comp_vec)
