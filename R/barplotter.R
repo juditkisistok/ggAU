383f94e76ee99f882a66fb390f5f3b61dc4ae0d0
juditkisistok <judit.kisistok@gmail.com>
2022-12-15 15:39:11 +0100

    implementing barplotter

--- R/barplotter.R
+++ R/barplotter.R
@@ -4,6 +4,48 @@
 #' @export
 #'
 #' @examples
-barplotter = function() {
+barplotter = function(data, x_val, y_val, order = NA, scale_labs = NA,
+                      pct = T, style = "light", colors = NA, y_lab = "y",
+                      x_lab = "x", title = "") {
+  data = data %>%
+    dplyr::select(!!x_val, !!y_val) %>%
+    dplyr::group_by(get(x_val), get(y_val)) %>%
+    dplyr::count() %>%
+    dplyr::group_by(`get(x_val)`) %>%
+    dplyr::mutate(x = signif(n / sum(n) * 100, digits = 3)) %>%
+    purrr::set_names(c('x', 'y', 'number', 'percent'))
+
+  fishers_df = data %>%
+    tidyr::pivot_wider(values_from = number, names_from = y, id_cols = x, values_fill = 0) %>%
+    tibble::column_to_rownames('x')
+
+  if (is.na(order)) {
+    order = c(unique(data$x))
+  }
+
+  if (is.na(scale_labs)) {
+    scale_labs = c(unique(data$x))
+  }
+
+  stat_title = paste0("Fisher's exact test p = ", signif(fisher.test(fishers_df)$p, digits = 3))
+
+  if (pct) {
+    p = ggpubr::ggbarplot(data, "x", "percent", lab.pos = "in", fill = "y",
+                          label = TRUE, order = order)
+  } else {
+    p = ggpubr::ggbarplot(data, "x", "number", lab.pos = "in", fill = "y",
+                          label = TRUE, order = order)
+  }
+
+  p = p +
+    scale_fill_au(discrete = T, style = style, colors = colors) +
+    ggplot2::ylab(y_lab) +
+    ggplot2::xlab(x_lab) +
+    ggplot2::ggtitle(label = title, subtitle = stat_title) +
+    ggplot2::theme(plot.title = ggplot2::element_text(hjust = 0.5),
+          plot.subtitle = ggplot2::element_text(hjust = 0.5)) +
+    ggplot2::scale_x_discrete(labels = scale_labs)
+
+  return (p)
 
 }
